998003b9d61a9930c3a9b0eeab39bff359e283c4
Abdul Mateen <mateen8325@gmail.com>
2025-05-23 11:15:57 +0500

    y

--- script.js
+++ script.js
@@ -164,4 +164,137 @@
 // strings methods in javascript
 
 
-// 1 
+// 1 toUpperCase toLowerCase
+
+
+let user = "hello world";
+let user2 = user.toUpperCase();
+console.log(user2);
+
+let user1 =  "HELLO WORLD";
+let user3 = user1.toLowerCase();
+console.log(user3);
+
+// 2 charAt
+let user4 = "hello world";
+let user5 = user4.charAt(0);
+console.log(user5);
+
+// 3 indexOf
+let user6 = "hello world";
+let user7 = user6.indexOf("o");
+console.log(user7);
+
+// 4 lastIndexOf
+let user8 = "hello world";
+let user9 = user8.lastIndexOf("o");
+console.log(user9);
+
+// 4 firstIndexOf
+let user10 = "hello world";
+let user11 = user10.indexOf("o");
+console.log(user11);    
+
+// 5 slice
+let user12 = "hello world";
+let user13 = user12.slice(0, 5);
+console.log(user13);
+
+// // 6 split
+// let user14 = "hello world";
+// let user15 = user14.split(" ");
+// console.log(user15);
+
+// // 6 split
+// let user14 = "hello world";
+// let user15 = user14.split("");
+// console.log(user15);
+
+// // 6 split
+// let user14 = "hello world";
+// let user15 = user14.split(",");
+// console.log(user15);
+
+
+// // 6 split
+// let user14 = "hello world";
+// let user15 = user14.split(" ");
+// console.log(user15);
+
+
+// // 7 replace
+let user16 = "hello world";
+let user17 = user16.replace("hello", "hi");
+console.log(user17);
+
+// // // 8 trim
+// let user18 = "   hello world   ";   
+// let user19 = user18.trim();
+// console.log(user19);
+
+// // 8 trim
+let user18 = "   hello world   ";   
+let user19 = user18;
+console.log(user19);
+
+
+// // // 9 concat
+// let user20 = "hello";
+// let user21 = "world";
+// let user22 = user20.concat(" ",user21);
+// console.log(user22);
+
+
+// // 9 concat
+let user20 = "hello";
+let user21 = "world";
+let user22 = user20.concat(user21);
+console.log(user22);
+
+// // // 10 includes
+// let user23 = "hello world";
+// let user24 = user23.includes("kk");
+// console.log(user24);
+
+
+// // 10 includes
+let user23 = "hello world";
+let user24 = user23.includes("hel");
+console.log(user24);
+
+// // // 11 startsWith
+// let user25 = "hello world";
+// let user26 = user25.startsWith("world");
+// console.log(user26);
+
+
+// // 11 startsWith
+let user25 = "hello world";
+let user26 = user25.startsWith("hello");
+console.log(user26);
+
+// // 12 endsWith
+let user27 = "hello world";
+let user28 = user27.endsWith("world");
+console.log(user28);
+
+
+
+// 13 search
+let user29 = "hello world";
+let user30 = user29.search("d")
+console.log(user30);
+
+// // 14 match
+// let user31 = "hello world";
+// let user32 = user31.match(/l/g)
+// console.log(user32);
+
+
+// 14 match
+let user31 = "hello world";
+let user32 = user31.match(/o/g)
+console.log(user32);
+console.log(user32.length);
+
+
